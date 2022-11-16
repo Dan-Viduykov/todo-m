ef3245576149915e573023ca92a2ef01e8495d73
Danya_Vid <danilviduykov@gmail.com>
2022-11-16 15:36:15 +0500

    add all needs components

--- src/components/screens/Home/Home.tsx
+++ src/components/screens/Home/Home.tsx
@@ -1,10 +1,18 @@
 import { FC } from "react";
+import Filter from "@/components/Filter";
+import TodoList from "@/components/TodoList";
+import AddTodoButton from "@/components/AddTodoButton";
 import styles from "./Home.module.scss";
 
 const Home: FC = () => {
     return (
-        <div>
-            Home
+        <div className={styles.wrap}>
+            <div className={styles.head}>
+                <h1 className={styles.title}>Todo list</h1>
+                <Filter className={styles.filter} />
+            </div>
+            <TodoList className={styles.todoList} />
+            <AddTodoButton className={styles.buttonAddTodo} />
         </div>
     )
 }
