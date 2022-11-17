3c808b9cb21836bae5966d4521dfea66c8282ad6
Danya_Vid <danilviduykov@gmail.com>
2022-11-17 12:27:38 +0500

    set up filter

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,16 +1,45 @@
-import { FC } from "react";
+import { FC, MouseEvent, useState } from "react";
 import styles from "./Filter.module.scss";
 
 interface FilterProps {
     className?: string;
 }
 
+const filterValues = [
+    { name: 'all', label: 'All' },
+    { name: 'done', label: 'Done' },
+    { name: 'undone', label: 'Undone' }
+]
+
 const Filter: FC<FilterProps> = ({ className }) => {
+    const [ activeFilter, setActiveFilter ] = useState('all')
+
+    const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
+        setActiveFilter(event.currentTarget.ariaLabel as string)
+    }
+
+    const elements = filterValues.map(({name, label}, index) => {
+        return (
+            <li 
+                key={index}
+                className={`
+                    ${styles.item}
+                    ${name === activeFilter ? styles.item_active : null}
+                `}
+            >    
+                <button
+                    className={styles.button}
+                    aria-label={name}
+                    onClick={handleClick}>
+                    {label}
+                </button>
+            </li>
+        )
+    })
+
     return (
         <ul className={`${styles.filter} ${className}`}>
-            <li className={styles.item}>all</li>
-            <li className={styles.item}>done</li>
-            <li className={styles.item}>undone</li>
+            {elements}
         </ul>
     )
 }
