eef79d6853e260b53a6e8afe073f5702c0b6e5b4
Danya_Vid <danilviduykov@gmail.com>
2022-11-23 21:10:36 +0500

    fix

--- src/components/UI/TextField/TextField.tsx
+++ src/components/UI/TextField/TextField.tsx
@@ -1,25 +1,30 @@
-import { FC, InputHTMLAttributes } from "react";
-import styles from "./TextField.module.scss";
+import { forwardRef, InputHTMLAttributes } from 'react';
+import styles from './TextField.module.scss';
 
-interface TextFieldProps extends InputHTMLAttributes<HTMLInputElement> {
-    className: string;
-    error?: boolean;
-    errorMessage?: string;
+export interface TextFieldProps extends InputHTMLAttributes<HTMLInputElement> {
+	label?: string;
+    className?: string;
+	error?: boolean;
+	errorMessage?: string;
 }
 
-const TextField: FC<TextFieldProps> = ({className, error, errorMessage, ...props}) => {
-    return (
-        <div className={`${styles.wrap} ${className}`}>
-            {errorMessage ? <span className={styles.errorMessage}>{errorMessage}</span> : null}
-            <input
-                className={`
-                    ${styles.input}
-                    ${error ? styles.input_error : null}
-                `}
-                {...props}
-            />
-        </div>
-    )
-}
+export const TextField = forwardRef<HTMLInputElement, TextFieldProps>(
+	({ className, error, errorMessage, label, ...props }, ref) => {
+		return (
+			<label className={className}>
+				{label && <span className={styles.label}>{label}</span>}
+				<input
+					ref={ref}
+					className={`
+						${styles.input}
+						${error && styles.input_error}
+					`}
+					{...props}
+				/>
+				{errorMessage && <span className={styles.errorMessage}>{errorMessage}</span>}
+			</label>
+		);
+	}
+);
 
-export default TextField
+TextField.displayName = 'TextField';
