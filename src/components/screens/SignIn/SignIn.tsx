df53f6423e3ab1f2edbf467ecacbe4ae7105cd85
Danya_Vid <danilviduykov@gmail.com>
2022-11-24 12:54:11 +0500

    add singIn

--- src/components/screens/SignIn/SignIn.tsx
+++ src/components/screens/SignIn/SignIn.tsx
@@ -1,30 +1,45 @@
-import TextField from "@/components/UI/TextField";
+import { FC } from "react";
+import axios from "axios";
 import Link from "next/link";
-import { ChangeEvent, FC, FormEvent, useState } from "react";
+import { useRouter } from "next/router";
+import { useForm } from "react-hook-form";
+
+import TextField from "@/components/UI/TextField";
 import styles from "./SignIn.module.scss";
+import userStore from "@/store/userStore";
 
 const SignIn: FC = () => {
-    const [ valueLogin, setValueLogin ] = useState('')
-    const [ valuePassword, setValuePassword ] = useState('')
+    const router = useRouter()
+    const { register, handleSubmit } = useForm();
+    
+    const loginUser = async (data: any) => {
+        await axios.post('http://localhost:8080/login', data)
+            .then(res => {
+                const data = res.data;
 
-    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
+                localStorage.setItem('user', JSON.stringify(data.user));
+                userStore.setUser(data.user);
+                router.push('/');
+            })
     }
 
     return (
         <section className={styles.wrap}>
             <h2 className={styles.title}>Вход в аккаунт</h2>
-            <form className={styles.form} onSubmit={handleSubmit}>
+            <form className={styles.form} onSubmit={handleSubmit(loginUser)}>
                 <TextField
                     className={styles.input}
-                    placeholder="Ваш логин"
-                    type="text"
-                />
+                    placeholder="Ваш email"
+                    type="email"
+                    {...register('email')}
+                    />
                 <TextField
                     className={styles.input}
                     placeholder="Ваш пароль"
                     type="password"
+                    {...register('password')}
                 />
+                <button className={styles.buttonSumbit}>войти</button>
             </form>
             <p className={styles.info}>
                 Ещё не зарегестрированы? 
